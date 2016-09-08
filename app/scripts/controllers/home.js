70023ea6368b86b8d4642ac175129ae8fb5d4c29
实训二 <526092392@qq.com>
2016-09-08 16:13:15 +0800

    第三个版本

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,3 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 1yeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 1yeomanApp
+ */
 angular.module('1yeoman')
   .controller('home', function () {
     
